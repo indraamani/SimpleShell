c53d4945b546922f65a298c70891350b5355be3c
indraamani <indraamani24@gmail.com>
2026-07-01 02:31:39 +0530

    add: fish, fastfetch

---
+++ .config/fish/config.fish
@@ -0,0 +1,30 @@
+fastfetch --config ~/.config/fastfetch/minimal.jsonc
+if status is-interactive
+    # No greeting
+    set fish_greeting
+
+    # Use starship
+    function starship_transient_prompt_func
+        starship module character
+    end
+    if test "$TERM" != "linux"
+        starship init fish | source
+        enable_transience
+    end
+    
+    # Colors
+    #if test -f ~/.local/state/quickshell/user/generated/terminal/sequences.txt
+    #    cat ~/.local/state/quickshell/user/generated/terminal/sequences.txt
+    #end
+
+    # Aliases
+    # kitty doesn't clear properly so we need to do this weird printing
+    alias clear "printf '\033[2J\033[3J\033[1;1H'"
+    alias celar "printf '\033[2J\033[3J\033[1;1H'"
+    alias claer "printf '\033[2J\033[3J\033[1;1H'"
+    
+    if test "$TERM" != "linux"
+        alias ls 'eza --icons'
+    end
+
+end
